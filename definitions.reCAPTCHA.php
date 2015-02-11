465e04f5a0f98151e0bc3c22befe1f10fb545074
mikeedev <mike@vriendenvanhetov.nl>
2015-02-11 11:34:58 +0100

    Create definitions.reCAPTCHA.php
    Translation for the plugin 'reCAPTCHA'.

---
+++ definitions.reCAPTCHA.php
@@ -0,0 +1,15 @@
+<?php
+
+														##########################################
+														## 	Copyright 2015 © Mike Schippers   	##
+														##########################################
+
+$definitions["Are you human?"] = "Ben je een mens?";
+$definitions["Private Key"] = "Priv&#233; sleutel";
+$definitions["Public Key"] = "Publiekelijke sleutel";
+$definitions["message.reCAPTCHARefreshInfo"] = "Klik of klik op de afbeelding om te vernieuwen";
+$definitions["message.invalidCAPTCHA"] = "Sorry, maar het komt niet overeen. Probeer het opnieuw.";
+$definitions["message.reCAPTCHA.settings"] = "Voer je CAPTCHA sleutels in (<a href='https://www.google.com/recaptcha/admin#whyrecaptcha' target='_blank'>Geen sleutels? Krijg ze hier!</a>)";
+$definitions["mlarray.reCAPTCHA"] = array(
+    "instructions_visual" => "Type de twee woorden"
+);
