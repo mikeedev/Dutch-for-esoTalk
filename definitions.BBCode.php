1034b8c9c44861e1737e45c5475ef9c5c3fd3ad0
mikeedev <mike@vriendenvanhetov.nl>
2015-02-11 11:31:46 +0100

    Create definitions.BBCode.php
    Translation for the included plugin 'BBCode'.

---
+++ definitions.BBCode.php
@@ -0,0 +1,14 @@
+<?php
+														##########################################
+														## 	Copyright 2015 © Mike Schippers   	##
+                            ##########################################
+														
+														// BBcode vertalingen
+
+$definitions["Code"] = "Code";
+$definitions["Image"] = "Afbeelding";
+$definitions["Link"] = "Link";
+$definitions["Strike"] = "Doorgestreept";
+$definitions["Header"] = "Header";
+$definitions["Italic"] = "Schuingedrukt";
+$definitions["Bold"] = "Vetgedrukt";
