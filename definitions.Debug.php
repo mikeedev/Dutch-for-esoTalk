13faa74f8474a5af02dba8f26f77351087d89783
mikeedev <mike@vriendenvanhetov.nl>
2015-02-11 11:32:54 +0100

    Create definitions.Debug.php
    MySQL translations.

---
+++ definitions.Debug.php
@@ -0,0 +1,11 @@
+<?php
+														##########################################
+														## 	Copyright 2015 © Mike Schippers   	##
+														##########################################
+														
+														// MySQL vertalingen
+
+$definitions["MySQL queries"] = "MySQL aanvragen";
+$definitions["Page loaded in %s seconds"] = "Pagina laadde in %s seconden";
+$definitions["POST + GET + FILES information"] = "POST + GET + FILES informatie";
+$definitions["SESSION + COOKIE information"] = "SESSIE + COOKIE informatie";
